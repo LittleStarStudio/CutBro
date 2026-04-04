0b4af00483ecfe497af5fff2cc934391eb122e8f
LittleStarStudio <studiolittlestar2@gmail.com>
2026-04-04 09:04:56 +0700

    feat: Pesan verify email && broken token tes

--- Backend/bootstrap/app.php
+++ Backend/bootstrap/app.php
@@ -24,8 +24,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-
         $middleware->alias([
             'verified.api' => EnsureEmailVerified::class,
             'token.expired' => EnsureTokenIsNotExpired::class,
